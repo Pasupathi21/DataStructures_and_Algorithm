19ded5b28b1fe1c38cae9786334ab40024d0aabe
Pasupathi21 <pasupathip396@gmail.com>
2024-05-07 18:21:53 +0530

    dfs algorithm logic

--- src/DSA/dfs.js
+++ src/DSA/dfs.js
@@ -9,12 +9,28 @@ const graph = {
 }
 
 let result = []
-function DFS(input){
-   const keys = Object.keys(input)
+let edgesIsExist = true
+function DFS(keys){
    for(let i =0; i < keys?.length; i++){
+
+    if(result.includes(keys[i])){
+        edgesIsExist = false
+        if(i + 1 === keys?.length){
+            return
+        }
+        continue
+    }
+
     if(!result.includes(keys[i])){
+        edgesIsExist = true
         result.push(keys[i])
-        
+    }
+
+    if(edgesIsExist){
+        DFS(graph[keys[i]])
     }
    }
 }
+
+DFS(Object.keys(graph))
+console.log("edges >>>>>>>>", result)
